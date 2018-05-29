0655000a8654105007c25759cf1ac03e04cc2a1e
lyswin2002 <lyswin2002@163.com>
2018-05-29 16:34:06 +0800

    ResponseResult对象添加info属性

--- src/main/java/com/elan/common/response/ResponseResult.java
+++ src/main/java/com/elan/common/response/ResponseResult.java
@@ -1,28 +1,51 @@
 package com.elan.common.response;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ResponseResult implements Serializable{
     private Integer code=-1;
     private String message="";
     private Object data;
+    private List info;
 
     public ResponseResult(){
-
+        this.code = -1;
+        this.message = "";
+        this.data = null;
+        this.info =null;
     }
     public ResponseResult(Object data){
         this.code = 0;
         this.message = "ok";
         this.data = data;
+        this.info =null;
+    }
+    public ResponseResult(Object data,List info){
+        this.code = 0;
+        this.message = "ok";
+        this.data = data;
+        this.info =info;
     }
     public ResponseResult(Integer code,String message){
         this.code = code;
         this.message = message;
+        this.data = null;
+        this.info = null;
+
     }
     public ResponseResult(Integer code,String message,Object data){
         this.code = code;
         this.message = message;
         this.data = data;
+        this.info = null;
+    }
+    public ResponseResult(Integer code,String message,Object data,List info){
+        this.code = code;
+        this.message = message;
+        this.data = data;
+        this.info = info;
     }
     public Integer getCode() {
         return code;
@@ -48,5 +71,11 @@ public class ResponseResult implements Serializable{
         this.data = data;
     }
 
+    public List getInfo() {
+        return info;
+    }
 
+    public void setInfo(List info) {
+        this.info = info;
+    }
 }
