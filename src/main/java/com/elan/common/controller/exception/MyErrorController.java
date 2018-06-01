3c719151309e6ca5951089626c2836647ecf9942
lyswin2002 <lyswin2002@163.com>
2018-06-01 15:16:21 +0800

    更新统一异常处理类：MyErrorController

--- src/main/java/com/elan/common/controller/exception/MyErrorController.java
+++ src/main/java/com/elan/common/controller/exception/MyErrorController.java
@@ -4,47 +4,104 @@ import com.elan.common.response.ResponseResult;
 import com.elan.common.response.ResponseResultUtils;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.web.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
-import java.util.Map;
+import javax.servlet.http.HttpServletResponse;
+import java.util.*;
 
 
 @Controller
-@RequestMapping("${server.error.path:${error.path:/error}}")
-public class MyErrorController implements ErrorController {
-    private final static Logger logger = LoggerFactory.getLogger(MyErrorController.class);
-     @Override
-        public String getErrorPath() {
-            return "/error";
-        }
+@RequestMapping("${server.error.path:/error}")
+public class MyErrorController  implements ErrorController  {
+    private Logger logger = LoggerFactory.getLogger(MyErrorController.class);
 
-        @RequestMapping
-        @ResponseBody
-        public ResponseResult doHandleError(HttpServletRequest req,Exception e)  {
-            ObjectMapper mapper = new ObjectMapper();
-            Map<String,Object>error = new HashMap<>();
-            error.put("url",req.getQueryString() ==null ? req.getRequestURI() :req.getRequestURI()+"?"+req.getQueryString());
-            error.put("method",req.getMethod());
-            error.put("param",req.getParameterMap());
-            String msg = (String)req.getAttribute("javax.servlet.error.message");
-            Integer code =(Integer)req.getAttribute("javax.servlet.error.status_code");
-           //String uri=(String) req.getAttribute("javax.servlet.forward.request_uri");
-           String error_uri=(String)req.getAttribute("javax.servlet.error.request_uri");
-            error.put("original_url",req.getQueryString() ==null ? error_uri :error_uri+"?"+req.getQueryString());
-            //将系统异常以打印出来
-            try {
-                logger.error("[错误]--"+mapper.writeValueAsString(error));
-            } catch (JsonProcessingException e1) {
-                e1.printStackTrace();
-            }
-            return ResponseResultUtils.error(code,msg,error);
+    @Value("${server.error.path:/error}")
+    private String errorPath;
+
+    @Value("${server.error.trace:true}")
+    private boolean isProduction;
+
+    @Autowired
+    private ErrorAttributes errorAttributes;
+
+    @RequestMapping(produces = {"text/html"})
+    public ModelAndView errorHtml(HttpServletRequest request, HttpServletResponse response) {
+        Map<String,Object> errorAttributes = getErrorAttributes(request, !this.isProduction);
+        Integer status=(Integer)errorAttributes.get("status");
+        String path=(String)errorAttributes.get("path");
+        String messageFound=(String)errorAttributes.get("message");
+        String message="";
+        String trace ="";
+        if(!StringUtils.isEmpty(path)){
+            message=String.format("Requested path %s with result %s",path,messageFound);
         }
+        if(!this.isProduction) {
+            trace = (String) errorAttributes.get("trace");
+            if(!StringUtils.isEmpty(trace)) {
+                message += String.format(" and trace %s", trace);
+            }
+        }
+        Map<String,Object> model = new HashMap<>();
+        model.put("status",status);
+        model.put("path",path);
+        model.put("message",message);
+        ModelAndView modelAndView = new ModelAndView();
+        logger.info(errorPath);
+        logger.info(message);
+        modelAndView.setViewName(errorPath);
+        modelAndView.addObject("error",model);
+        return modelAndView;
+    }
+
+    @RequestMapping(headers = {"X-Requested-With"},produces = {MediaType.APPLICATION_JSON_VALUE})
+    @ResponseBody
+    public ResponseResult error(HttpServletRequest request, HttpServletResponse response) {
+        return buildBody(request, !this.isProduction);
+
+    }
+
+    private ResponseResult buildBody(HttpServletRequest request,Boolean includeStackTrace){
+        Map<String,Object> errorAttributes = getErrorAttributes(request, includeStackTrace);
+        Integer status=(Integer)errorAttributes.get("status");
+        String path=(String)errorAttributes.get("path");
+        String messageFound=(String)errorAttributes.get("message");
+        String message="";
+        String trace ="";
+        if(!StringUtils.isEmpty(path)){
+            message=String.format("Requested path %s with result %s",path,messageFound);
+        }
+        if(includeStackTrace) {
+            trace = (String) errorAttributes.get("trace");
+            if(!StringUtils.isEmpty(trace)) {
+                message += String.format(" and trace %s", trace);
+            }
+        }
+        return new ResponseResult(status,message);
+    }
+
+    @Override
+    public String getErrorPath() {
+        return errorPath;
+    }
+    private Map<String, Object> getErrorAttributes(HttpServletRequest request, boolean includeStackTrace) {
+        RequestAttributes requestAttributes = new ServletRequestAttributes(request);
+        return errorAttributes.getErrorAttributes(requestAttributes, includeStackTrace);
+    }
 
 }
